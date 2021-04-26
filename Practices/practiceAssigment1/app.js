cc2ddcfdbd73c58816f7f37df93ddf17d4fcdb99
BarrierHT <facuro1000@outlook.com>
2021-04-26 00:33:08 -0300

    Practices ignored

--- Practices/practiceAssigment1/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const routesMain = require('./main.js');
-
-console.log(routesMain.text);
-const server = http.createServer(routesMain.handler);
-
-server.listen(3000);
--- Practices/practiceAssigment3/public/js/main.js
+++
@@ -1,2 +0,0 @@
-alert('Hi from the client(alert) JS');
-console.log('Hi from the client(console) JS');
